8bdb311d74082eb7abbbba5b31a24671a04a4384
Aditeya Baral <aditeya.baral@gmail.com>
2020-07-17 12:17:54 +0530

    Added getVocabularySize()

--- dep.c
+++ dep.c
@@ -110,8 +110,39 @@ int getVocabularySize(char* corpus)
     int len = strlen(corpus);
     char* temp1 = (char*)malloc(sizeof(char)*len);
     char* temp2 = (char*)malloc(sizeof(char)*len);
+    char* temp3 = (char*)malloc(sizeof(char)*len);
     strcpy(temp1, corpus);
+    strcpy(temp2, "");
+    strcpy(temp3, "");
     char* token1;
+    char* token2;
+    char* save1;
+    char* save2;
+    int V = 0;
+    
+    token1 = strtok_r(temp1, " ", &save1);
+    while(token1 != NULL)
+    {
+        strcpy(temp3, "");
+        strcpy(temp3, temp2);        
+        token2 = strtok_r(temp3, " ", &save2);
+        bool flag = false;
+        while(token2!=NULL)
+        {
+            if (!strcmp(token1, token2))
+            flag = true;
+            token2 = strtok_r(NULL, " ", &save2);
+        }
+        if (!flag)
+        {
+            strcat(temp2, token1);
+            strcat(temp2, " ");
+            V++;
+        }
+        token1 = strtok_r(NULL, " ", &save1);
+    }
+    //printf("Vocab: %s\n", temp2);
+    return V;
 }
 
 void OneHotEncoding(EMBEDDING* model, char* corpus)
@@ -130,7 +161,6 @@ void OneHotEncoding(EMBEDDING* model, char* corpus)
         insert(node, model);
     }
 }
-
 
 void train(EMBEDDING* model, int C, int N, float alpha, char* corpus)
 {
--- test.c
+++ test.c
@@ -1,28 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-char* trim(char* word)
+#include <stdbool.h>
+int getVocabularySize(char* corpus)
 {
-    char* s = (char*)malloc(sizeof(char)*50);
-    int pos1 = 0, pos2 = 0;
-    while(word[pos1]==' ')
-        pos1++;
-    while(word[pos1]!=' ' && word[pos1]!='\0')
+    int len = strlen(corpus);
+    char* temp1 = (char*)malloc(sizeof(char)*len);
+    char* temp2 = (char*)malloc(sizeof(char)*len);
+    char* temp3 = (char*)malloc(sizeof(char)*len);
+    strcpy(temp1, corpus);
+    strcpy(temp2, "");
+    strcpy(temp3, "");
+    char* token1;
+    char* token2;
+    char* save1;
+    char* save2;
+    int V = 0;
+    
+    token1 = strtok_r(temp1, " ", &save1);
+    while(token1 != NULL)
     {
-        s[pos2] = word[pos1];
-        pos1++;
-        pos2++;
+        strcpy(temp3, "");
+        strcpy(temp3, temp2);        
+        token2 = strtok_r(temp3, " ", &save2);
+        bool flag = false;
+        while(token2!=NULL)
+        {
+            if (!strcmp(token1, token2))
+            flag = true;
+            token2 = strtok_r(NULL, " ", &save2);
+        }
+        if (!flag)
+        {
+            strcat(temp2, token1);
+            strcat(temp2, " ");
+            V++;
+        }
+        token1 = strtok_r(NULL, " ", &save1);
     }
-    s[pos2] = '\0';
-    return s;
+    //printf("Vocab: %s\n", temp2);
+    return V;
 }
 
 int main()
 {
     char *s = (char*)malloc(sizeof(char)*1000);
     scanf("%[^\\0]s", s);
-    printf("%s\n", s);
-    char* trimmed = trim(s);
-    printf("%s\n", trimmed);
+    printf("Input: %s\n", s);
+    int V = getVocabularySize(s);
+    printf("Size: %d\n", V);
     return 0;
 }
