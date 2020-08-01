3567aae20ce20e84afcca10be023448fd39cc60a
Aditeya Baral <aditeya.baral@gmail.com>
2020-08-01 20:01:13 +0530

    Update w2v.c

--- Source Code/w2v.c
+++ Source Code/w2v.c
@@ -2,21 +2,24 @@
 
 int main()
 {
-    bool train = false;
-    #if train
-    char *s = (char*)malloc(sizeof(char)*INT_MAX);
-    scanf("%[^\\0]s", s);
-    EMBEDDING* model = createModel();
-    train(model, s, 3, 300, 5, 10, 1, true);
-    saveModel(model, false);
-    destroyModel(model);
-    free(s);
-    #endif
+    bool train_model = true;
 
-    #if !train
-    //EMBEDDING* model = loadModelEmbeddings("model-embeddings.csv");
-    EMBEDDING* model = loadModelForTraining("model-embeddings.csv", NULL, NULL, NULL, NULL, NULL, NULL);
-    destroyModel(model);
-    #endif
+    if (train_model)
+    {
+        char *s = (char*)malloc(sizeof(char)*INT_MAX);
+        scanf("%[^\\0]s", s);
+        EMBEDDING* model = createModel();
+        train(model, s, 3, 300, 5, 10, 1, true);
+        saveModel(model, false);
+        destroyModel(model);
+        free(s);
+    }
+
+    else
+    {
+        //EMBEDDING* model = loadModelEmbeddings("model-embeddings.csv");
+        EMBEDDING* model = loadModelForTraining("model-embeddings.csv", NULL, NULL, NULL, NULL, NULL, NULL);
+        destroyModel(model);
+    }
     return 0;  
 }
