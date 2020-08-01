99416966e18267f7ccb180524e8039cc0a92f6a1
Aditeya Baral <aditeya.baral@gmail.com>
2020-08-01 22:14:02 +0530

    Update w2v.c

--- Source Code/w2v.c
+++ Source Code/w2v.c
@@ -10,6 +10,7 @@ int main(int argc, char* argv[])
         scanf("%[^\\0]s", s);
         EMBEDDING* model = createModel();
         train(model, s, 3, 300, 5, 2, 1, true);
+        displayArray(model->W1, model->dimension, model->vocab_size);
         saveModel(model, false);
         destroyModel(model);
         free(s);
@@ -23,7 +24,7 @@ int main(int argc, char* argv[])
     else
     {
         EMBEDDING* model = loadModelForTraining("model-embeddings.csv", "model-X.csv", "model-y.csv", "model-weights-w1.csv", "model-weights-w2.csv", "model-bias-b1.csv", "model-bias-b2.csv");
-        //destroyModel(model);
+        destroyModel(model);
     }
     return 0;  
 }
